61b2d5635c83f81554c9dbb1336fcb4a8b9b6954
panchaliSam <panchalisam20030117@gmail.com>
2025-03-30 21:58:43 +0530

    Chnage the db

--- server/restaurant-service/src/repository/restaurantRepository.js
+++ server/restaurant-service/src/repository/restaurantRepository.js
@@ -12,6 +12,31 @@ const RestaurantRepository = {
         const query = 'SELECT * FROM Restaurants WHERE RestaurantName = ?';
         const [rows] = await pool.execute(query, [restaurantName]);
         return rows;
+    },
+
+    findById: async(id) => {
+        const query = 'SELECT * FROM Restaurants WHERE RestaurantId = ?';
+        const [rows] = await pool.execute(query, [id]);
+        return rows;
+    },
+
+    getAllRestaurants: async () => {
+        const query = 'SELECT * FROM Restaurants';
+        const [rows] = await pool.execute(query);
+        return rows;
+    },
+
+    deleteBuId: async(id, restaurantData) => {
+        const query  = 'DELETE FROM Restaurants WHERE RestaurantId = ?';
+        const [result] = await pool.execute(query, [id]);
+        return result;
+    },
+
+    updateById: async (id, restaurantData) => {
+        const { restaurantName, address, phone, email, avatar } = restaurantData;
+        const query = 'UPDATE Restaurants SET RestaurantName = ?, ADDRESS = ?,  Phone= ?, Email = ?, Availability = ? WHERE RestaurantId = ?';
+        const [result] = await pool.execute(query, [id, restaurantName, address, phone, email, availability]);
+        return result;
     }
 }
 
