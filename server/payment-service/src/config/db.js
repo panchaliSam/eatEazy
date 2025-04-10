36c923a97113fe4e4ae9aa02c5c891e5a346d1c0
IT22053282 <it22053282@my.sliit.lk>
2025-04-10 19:40:00 +0530

    Updated db.js

--- server/payment-service/src/config/db.js
+++ server/payment-service/src/config/db.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql2/promise');
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require('./env');
+require('dotenv').config();
 
 
 const pool = mysql.createPool({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT), 
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0,
-});
+    queueLimit: 0
+  });
 
 const connectToDatabase = async () => {
     try {
