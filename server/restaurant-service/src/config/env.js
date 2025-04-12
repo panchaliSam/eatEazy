56e682d1f01fc8bbfef04982f8df84dc6bde205b
panchaliSam <panchalisam20030117@gmail.com>
2025-04-12 07:14:00 +0530

    Add db port

--- server/restaurant-service/src/config/env.js
+++ server/restaurant-service/src/config/env.js
@@ -5,6 +5,7 @@ module.exports = {
     DB_USER: process.env.DB_USER,
     DB_PASSWORD: process.env.DB_PASSWORD,
     DB_NAME: process.env.DB_NAME,
+    DB_PORT: process.env.DB_PORT,
     AUTH_SERVICE_PORT: process.env.AUTH_SERVICE_PORT,
     RESTAURANT_SERVICE_PORT: process.env.RESTAURANT_SERVICE_PORT,
     API_GATEWAY_AUTH_SERVICE_URL: process.env.API_GATEWAY_AUTH_SERVICE_URL,
