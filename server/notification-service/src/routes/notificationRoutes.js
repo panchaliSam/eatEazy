03eac4d082077a77aa9bbda7df685bb9bd77c6f8
IT22053282 <it22053282@my.sliit.lk>
2025-04-13 19:47:54 +0530

    Add getMyNotifications

--- server/notification-service/src/routes/notificationRoutes.js
+++ server/notification-service/src/routes/notificationRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const NotificationController = require('../controllers/notificationController');
+const authMiddleware = require('../middleware/auth');
 
 router.post('/create', NotificationController.createNotification);
 router.post('/send-email', NotificationController.sendEmailNotification);
 router.post('/send-sms', NotificationController.sendSMSNotification);
+router.get('/my', authMiddleware, NotificationController.getMyNotifications);
 
 module.exports = router;
