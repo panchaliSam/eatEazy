b05e632ed898220f371a09a716e8e73dbe76859b
panchaliSam <panchalisam20030117@gmail.com>
2025-04-17 10:17:10 +0530

    Implemet delete restarant by id

--- client/src/utils/api/RestaurantApi.ts
+++ client/src/utils/api/RestaurantApi.ts
@@ -1,13 +1,7 @@
 import axios from "axios";
 import { API_URL } from "./ApiURL";
 import { getAuthHeaders } from "../helper/AuthHelper";
-import {
-  getAccessToken,
-  getRefreshToken,
-  setTokens,
-  clearTokens,
-} from "../helper/TokenHelper";
-import { get } from "http";
+import { getAccessToken } from "../helper/TokenHelper";
 
 interface RestaurantData {
   restaurantName?: string;
@@ -84,6 +78,62 @@ const RestaurantApi = {
       if (axios.isAxiosError(error) && error.response) {
         throw new Error(
           error.response.data.message || "Failed to fetch restaurant."
+        );
+      }
+      throw new Error("An unexpected error occurred.");
+    }
+  },
+
+  updateRestaurantById: async (
+    restaurantId: string,
+    restaurantData: RestaurantData
+  ) => {
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      throw new Error("No access token found. Please log in again.");
+    }
+    try {
+      const response = await axios.put(
+        `${API_URL}/restaurants/${restaurantId}`,
+        restaurantData,
+        {
+          headers: {
+            ...getAuthHeaders(),
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new Error(
+          error.response.data.message || "Failed to update restaurant."
+        );
+      }
+      throw new Error("An unexpected error occurred.");
+    }
+  },
+
+  deleteRestaurantById: async (restaurantId: string) => {
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      throw new Error("No access token found. Please log in again.");
+    }
+    try {
+      const response = await axios.delete(
+        `${API_URL}/restaurants/${restaurantId}`,
+        {
+          headers: {
+            ...getAuthHeaders(),
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new Error(
+          error.response.data.message || "Failed to delete restaurant."
         );
       }
       throw new Error("An unexpected error occurred.");
