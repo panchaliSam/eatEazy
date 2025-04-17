562b4200d151539833fc1c420d6b55db7d65d95b
panchaliSam <panchalisam20030117@gmail.com>
2025-04-17 09:31:35 +0530

    Implemet restatrant register api

---
+++ client/src/utils/api/RestaurantApi.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { API_URL } from "./ApiURL";
+import { getAuthHeaders } from "../helper/AuthHelper";
+import {
+  getAccessToken,
+  getRefreshToken,
+  setTokens,
+  clearTokens,
+} from "../helper/TokenHelper";
+
+interface RestaurantData {
+  restaurantName?: string;
+  address?: string;
+  phone?: string;
+  availability?: string;
+}
+
+const RestaurantApi = {
+  registerRestaurant: async (restaurantData: RestaurantData) => {
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      throw new Error("No access token found.");
+    }
+    try {
+      const response = await axios.post(
+        `${API_URL}/restaurants/register`,
+        restaurantData,
+        {
+          headers: {
+            ...getAuthHeaders(),
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        throw new Error(
+          error.response.data.message || "Failed to register restaurant."
+        );
+      }
+      throw new Error("An unexpected error occurred.");
+    }
+  },
+};
+
+export default RestaurantApi;
