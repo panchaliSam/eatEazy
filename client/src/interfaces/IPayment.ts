a91b3123b2b3f564a638ff51460906bfb96833f0
IT22053282 <it22053282@my.sliit.lk>
2025-04-23 20:26:53 +0530

    Payment and notification interfaces and api files created.

---
+++ client/src/interfaces/IPayment.ts
@@ -0,0 +1,29 @@
+export interface IPayment {
+    PaymentID?: number;
+    OrderID: number;
+    Amount: number;
+    PaymentMethod: 'PayHere';
+    paymentStatus?: 'Pending' | 'Completed' | 'Failed'; 
+    TransactionID?: string;
+    PaymentDate?: string; 
+  }
+
+  export interface IPayHerePayload {
+    merchant_id: string;
+    return_url: string;
+    cancel_url: string;
+    notify_url: string;
+    order_id: string;
+    items: string;
+    payhere_amount: number;
+    payhere_currency: 'LKR';
+    first_name: string;
+    last_name: string;
+    email: string;
+    phone: string;
+    address: string;
+    city: string;
+    country: string;
+  }
+  
+  
