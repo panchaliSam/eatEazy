a1fecc7d79ae6896f777e946d244513f45e5491d
Kumodi Bogahawatte <127733868+KumodiBogahawatte@users.noreply.github.com>
2025-04-28 16:09:42 +0530

    Update notificationTemplates.js

--- server/notification-service/src/services/notificationTemplates.js
+++ server/notification-service/src/services/notificationTemplates.js
@@ -1,5 +1,7 @@
+// services/notificationTemplates.js
 module.exports = {
   emailTemplates: {
+    // Existing templates
     orderConfirmation: (data) => ({
       subject: "Order Confirmation",
       text: `Hi ${data.customerName}, your order ${data.orderId} was confirmed!`
@@ -42,10 +44,61 @@ module.exports = {
     deliveryAssigned: (data) => ({
       subject: "Delivery Assigned",
       text: `Hi ${data.customerName}, your order ${data.orderId} is out for delivery by ${data.driverName}.`
+    }),
+    
+    // New templates
+    deliveryPicked: (data) => ({
+      subject: "Your Order is on the Way",
+      text: `Hi ${data.customerName},
+      
+Your order #${data.orderId} has been picked up by ${data.driverName} and is on the way to you.
+      
+Estimated delivery time: ${data.eta || 'soon'}
+      
+Thank you for using EatEazy!`
+    }),
+    
+    orderUpdate: (data) => ({
+      subject: `Order #${data.orderId} Status Update`,
+      text: `Hi ${data.customerName},
+      
+The status of your order #${data.orderId} has been updated to: ${data.status}
+      
+${data.additionalInfo || ''}
+      
+Thank you for using EatEazy!`
+    }),
+    
+    welcomeUser: (data) => ({
+      subject: "Welcome to EatEazy!",
+      text: `Hi ${data.name},
+      
+Welcome to EatEazy! We're thrilled to have you join our platform.
+      
+Explore restaurants near you and enjoy delicious meals delivered right to your doorstep.
+      
+Happy ordering!
+The EatEazy Team`
+    }),
+    
+    passwordReset: (data) => ({
+      subject: "Password Reset Request",
+      text: `Hi ${data.name},
+      
+We've received a request to reset your password. To proceed, please use the following code:
+      
+${data.resetCode}
+      
+This code will expire in 15 minutes.
+      
+If you didn't request this change, please ignore this email or contact support.
+      
+The EatEazy Team`
     })
   },
 
   smsTemplates: {
+    // Existing templates
     orderConfirmation: (data) =>
       `EatEazy: Order #${data.orderId} confirmed. ETA: ${data.eta}.`,
     paymentSuccess: (data) =>
@@ -57,6 +110,22 @@ module.exports = {
     orderCancelled: (data) =>
       `EatEazy: Order #${data.orderId} was cancelled. ${data.reason || ''}`,
     deliveryAssigned: (data) =>
-      `EatEazy: Your order #${data.orderId} is out for delivery by ${data.driverName}.`
+      `EatEazy: Your order #${data.orderId} is out for delivery by ${data.driverName}.`,
+      
+    // New templates
+    deliveryPicked: (data) =>
+      `EatEazy: Your order #${data.orderId} has been picked up and is on the way. ETA: ${data.eta || 'soon'}`,
+      
+    orderUpdate: (data) =>
+      `EatEazy: Order #${data.orderId} update: ${data.status}`,
+      
+    welcomeUser: (data) =>
+      `Welcome to EatEazy, ${data.name}! Your food delivery app for convenient ordering.`,
+      
+    passwordReset: (data) =>
+      `EatEazy: Your password reset code is ${data.resetCode}. Valid for 15 minutes.`,
+      
+    driverAssignment: (data) =>
+      `EatEazy: New delivery assigned! Order #${data.orderId} from ${data.restaurantName}. Pickup at: ${data.restaurantAddress}`
   }
 };
