f2f937d60a55e644b4449ba4bc791189fdb2461e
panchaliSam <panchalisam20030117@gmail.com>
2025-04-28 17:55:06 +0530

    Remove the react import

--- client/src/components/customer/RestaurantContent.tsx
+++ client/src/components/customer/RestaurantContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
